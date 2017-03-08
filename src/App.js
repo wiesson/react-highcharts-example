b76b9c61e60eafbf8d51ff2d091aadcb17c14512
Arne Wiese <arne.wiese@gmail.com>
2017-03-08 12:38:40 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Highcharts from 'highcharts';
+
+class Chart extends Component {
+    componentDidMount() {
+        this.chart = new Highcharts[this.props.type || "Chart"](
+            this.props.container,
+            this.props.options
+        );
+    }
+
+    componentWillUnmount() {
+        this.chart.destroy();
+    }
+
+    render() {
+        return (
+            <div id={this.props.container}></div>
+        )
+    }
+}
+
+class App extends Component {
+    render() {
+
+        const options = {
+            title: {
+                text: 'Fruit Consumption'
+            },
+            xAxis: {
+                categories: ['Apples', 'Bananas', 'Oranges']
+            },
+            yAxis: {
+                title: {
+                    text: 'Fruit eaten'
+                }
+            },
+            chart: {
+                type: 'bar'
+            },
+            series: [{
+                name: 'Jane',
+                data: [1, 0, 4]
+            }, {
+                name: 'John',
+                data: [5, 7, 3]
+            }]
+        };
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+
+                <Chart container={'chart'} options={options} />
+            </div>
+        );
+    }
+}
+
+export default App;
