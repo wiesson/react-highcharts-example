291b4dac99b3681fb887a92bb57e52c3e82ef5d4
Arne Wiese <arne.wiese@gmail.com>
2017-09-07 18:22:06 +0200

    Reformatted files

--- src/App.js
+++ src/App.js
@@ -1,67 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Highcharts from 'highcharts';
 
 class Chart extends Component {
-    componentDidMount() {
-        this.chart = new Highcharts[this.props.type || "Chart"](
-            this.refs.chart,
-            this.props.options
-        );
-    }
+  componentDidMount() {
+    this.chart = new Highcharts[this.props.type || 'Chart'](
+      this.refs.chart,
+      this.props.options
+    );
+  }
 
-    componentWillUnmount() {
-        this.chart.destroy();
-    }
+  componentWillUnmount() {
+    this.chart.destroy();
+  }
 
-    render() {
-        return (
-            <div ref="chart"/>
-        )
-    }
+  render() {
+    return <div ref="chart" />;
+  }
 }
 
 class App extends Component {
-    render() {
-        const options = {
-            title: {
-                text: 'Fruit Consumption'
-            },
-            xAxis: {
-                categories: ['Apples', 'Bananas', 'Oranges']
-            },
-            yAxis: {
-                title: {
-                    text: 'Fruit eaten'
-                }
-            },
-            chart: {
-                type: 'bar'
-            },
-            series: [{
-                name: 'Jane',
-                data: [1, 0, 4]
-            }, {
-                name: 'John',
-                data: [5, 7, 3]
-            }]
-        };
+  render() {
+    const options = {
+      title: {
+        text: 'Fruit Consumption'
+      },
+      xAxis: {
+        categories: [
+          'Apples',
+          'Bananas',
+          'Oranges',
+          'Pineapples',
+          'Blueberries'
+        ]
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      chart: {
+        type: 'line'
+      },
+      series: [
+        {
+          name: 'Jane',
+          data: [1, 0, 4, 0, 3]
+        },
+        {
+          name: 'John',
+          data: [5, 7, 3, 2, 4]
+        },
+        {
+          name: 'Doe',
+          data: [0, 0, 0, 1, 0]
+        }
+      ]
+    };
 
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
 
-                <Chart options={options}/>
-            </div>
-        );
-    }
+        <Chart options={options} />
+      </div>
+    );
+  }
 }
 
 export default App;
